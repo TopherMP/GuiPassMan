3d4ef311ca555270543e463264e9857f42842fef
TopherMP <77714597+TopherMP@users.noreply.github.com>
2024-09-27 15:29:08 -0300

    Gestor de contraseñas encriptadas por rsa

---
+++ funciones.py
@@ -0,0 +1,129 @@
+from tkinter import messagebox
+import utils
+import base64
+import rsa
+
+# JSON donde se almacenan los datos
+dictJson = utils.load_json("passwords.json", {})
+
+# Read the files
+with open("public.pem","rb") as pub:
+    public = rsa.PublicKey.load_pkcs1(pub.read())
+
+# Funciones CRUD
+def create_data(nameEntry, userEntry, passEntry, treeview):
+    app_Name = nameEntry.get()
+    user_Mail_App = userEntry.get()
+    pass_App = passEntry.get()
+
+    passCrypt = utils.encrypt(public, pass_App)
+    passB64 = base64.b64encode(passCrypt).decode("utf-8")
+
+    # Verificar que los campos de texto no estén vacíos
+    if not app_Name or not user_Mail_App or not pass_App:
+        messagebox.showwarning("Advertencia", "Tienes que completar todos los datos")
+        return
+
+    # Verificar si la aplicación ya está en el JSON
+    if app_Name in dictJson:
+        messagebox.showwarning("Advertencia", "El nombre de la app ya lo tienes agregado, por favor, cambia el nombre")
+        return
+
+    # Agregar en el diccionario
+    dictJson[app_Name] = {
+        "User": user_Mail_App,
+        "Password": passB64
+    }
+
+    # Guardar datos en el archivo JSON
+    utils.save_json("passwords.json", dictJson)
+
+    # Actualizar el TreeView
+    update_treeview(treeview)
+
+    # Limpiar Entry
+    utils.clean_entries(nameEntry, userEntry, passEntry)
+
+# Actualizar datos
+def update_data(nameEntry, userEntry, passEntry, treeview):
+    app_Name = nameEntry.get()
+    user_Mail_App = userEntry.get()
+    pass_App = passEntry.get()
+
+    passCrypt = utils.encrypt(public, pass_App)
+    passB64 = base64.b64encode(passCrypt).decode("utf-8")
+
+    # Verificar que los campos de texto no estén vacíos
+    if not app_Name or not user_Mail_App or not pass_App:
+        messagebox.showwarning("Advertencia", "Tienes que seleccionar una cuenta para editar")
+        return
+
+    selected_item = treeview.selection()
+
+    if selected_item:
+        values = treeview.item(selected_item)['values']
+        original_App_Name = values[0]
+
+        # Eliminar el nombre original si ha cambiado
+        if original_App_Name != app_Name:
+            del dictJson[original_App_Name]
+
+        dictJson[app_Name] = {
+            "User": user_Mail_App,
+            "Password": passB64
+        }
+
+        # Guardar datos actualizados
+        utils.save_json("passwords.json", dictJson)
+
+        # Actualizar el TreeView
+        update_treeview(treeview)
+
+        # Limpiar Entry
+        utils.clean_entries(nameEntry, userEntry, passEntry)
+
+        # Mostrar mensaje de actualización
+        messagebox.showinfo("Actualización", f'Datos actualizados para "{app_Name}":\nUsuario: {dictJson[app_Name]["User"]}, Contraseña: {dictJson[app_Name]["Password"]}')
+
+    else:
+        messagebox.showwarning("Advertencia", "Selecciona un elemento de la lista para actualizar")
+
+# Eliminar un dato
+def delete_data(treeview, nameEntry, userEntry, passEntry):
+    selected_index = treeview.selection()
+
+    if selected_index:
+        item = treeview.item(selected_index)
+        app_Name = item["values"][0]
+        
+        delete=messagebox.askokcancel("Eliminar dato","Estas seguro que deseas eliminar")
+        if delete:
+            # Verificar si el nombre del item seleccionado existe en el diccionario
+            if app_Name in dictJson:
+                del dictJson[app_Name]
+
+                # Guardar los cambios en el archivo JSON
+                utils.save_json("passwords.json", dictJson)
+
+                # Actualizar el TreeView
+                update_treeview(treeview)
+
+                # Limpiar Entry
+                utils.clean_entries(nameEntry, userEntry, passEntry)
+                messagebox.showinfo("Eliminado","Elemento eliminado con éxito")
+            else:
+                messagebox.showwarning("Advertencia", "El elemento seleccionado no existe")
+        else:
+            messagebox.showinfo("Cancelado","Has cancelado la operación")
+    else:
+        messagebox.showwarning("Advertencia", "Selecciona un elemento de la lista para eliminar")
+
+# Actualizar tabla
+def update_treeview(treeview):
+    # Limpiar el Treeview antes de agregar nuevos datos
+    for row in treeview.get_children():
+        treeview.delete(row)
+
+    # Insertar los datos del JSON en el Treeview
+    for app, data in dictJson.items():
+        treeview.insert("", "end", values=(app, data["User"], data["Password"]))
---
+++ utils.py
@@ -0,0 +1,54 @@
+import json
+import tkinter as tk
+from tkinter import messagebox
+import rsa
+import os
+
+if not os.path.exists("public.pem") or not os.path.exists("private.pem"):
+    # generate new rsa keys
+    public, private = rsa.newkeys(2048)
+
+    # Write the keys in respective files
+    with open("public.pem","wb") as pub:
+        pub.write(public.save_pkcs1())
+    with open("private.pem","wb") as priv:
+        priv.write(private.save_pkcs1())
+
+    #with open("PassMan.psman","wb") as file:
+    #    file.write(public.save_pkcs1()+private.save_pkcs1())
+
+else:
+    # Read the files
+    with open("public.pem","rb") as pub:
+        public = rsa.PublicKey.load_pkcs1(pub.read())
+    with open("private.pem","rb") as priv:
+        private = rsa.PrivateKey.load_pkcs1(priv.read())
+
+def encrypt(public_key, pswrd):
+    encryptfile = rsa.encrypt(pswrd.encode(), public_key)
+    return encryptfile
+
+# Función para cargar datos desde un archivo JSON
+def load_json(filename,data):
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        with open(filename, 'w') as file: json.dump(data, file, indent=4)
+        return {}
+
+# Función para guardar datos en un archivo JSON
+def save_json(filename, data):
+    try:
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+    except Exception as e:
+        messagebox.showerror("Error", f"No se pudo guardar el archivo '{filename}': {str(e)}")
+
+# Función para verificar si los campos de entrada están vacíos
+def validate_entries(*entries):
+    return all(entry.get().strip() != "" for entry in entries)
+
+def clean_entries(*entries):
+    for entry in entries:
+        entry.delete(0,tk.END)
